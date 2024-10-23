66c63304d63eb10412841494b17b0d780c31fb63
LAICHEEHOONG <laicheehoong@gmail.com>
2024-10-23 15:15:38 +0800

    try payment

---
+++ app/api/webhook/route.js
@@ -0,0 +1,27 @@
+import Stripe from "stripe";
+import { NextResponse, NextRequest } from "next/server";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+export async function POST(req) {
+  const payload = await req.text();
+  const res = JSON.parse(payload);
+
+  const sig = req.headers.get("Stripe-Signature");
+
+  const dateTime = new Date(res?.created * 1000).toLocaleDateString();
+  const timeString = new Date(res?.created * 1000).toLocaleDateString();
+
+  try {
+    let event = stripe.webhooks.constructEvent(
+      payload,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    console.log("event", event.type);
+
+    return NextResponse.json({ status: "success", even: event.type });
+  } catch (error) {
+    return NextResponse.json({ status: "Failed", error });
+  }
+}
