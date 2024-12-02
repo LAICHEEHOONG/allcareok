ed871c8a6a2868be60bb56614bdc7b54d137d989
LAICHEEHOONG <laicheehoong@gmail.com>
2024-12-02 22:35:05 +0800

    editor d map

--- components/editor/MapCard.js
+++ components/editor/MapCard.js
@@ -1,52 +1,4 @@
-// import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { setFocus } from "@/redux/features/editor/editorSlice";
-// import { GoogleMapsEmbed } from "@next/third-parties/google";
-
-// export default function MapCard() {
-//   const dispatch = useDispatch();
-//   const cardFocus = useSelector((state) => state.editor?.cardFocus);
-
-//   return (
-//     <Card
-//       className={` m-5 p-1 w-[300px] ${
-//         cardFocus === "map" ? "border-solid border-2 border-black" : ""
-//       } `}
-//       isPressable
-//       onPress={() => {
-//         dispatch(setFocus("map"));
-//       }}
-//     >
-//       <CardBody className="">
-//         <div className="flex flex-col justify-center items-center">
-//           <div className="font-medium self-start">Location</div>
-//           <GoogleMapsEmbed
-//             apiKey="AIzaSyBvSmftem94fcOEtNH9qUDn9ESrw-8AtHk"
-//             height={200}
-//             width="100%"
-//             mode="place"
-//             q="Brooklyn+Bridge,New+York,NY"
-//           />
-//           {/* <div className="text-small tracking-tight text-default-400 self-start">
-//             8 photos
-//           </div> */}
-//           {/*
-//           <Image
-//             // className="min-w-[37px] "
-
-//             className="flex justify-center items-center mt-4"
-//             width={350}
-//             // radius="none"
-//             alt="photos upload poster"
-//             src="/images/poster_sample.png"
-//           /> */}
-//         </div>
-//       </CardBody>
-//     </Card>
-//   );
-// }
-
-import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
 import { useSelector, useDispatch } from "react-redux";
 import { setFocus } from "@/redux/features/editor/editorSlice";
 import { GoogleMapsEmbed } from "@next/third-parties/google";
@@ -68,17 +20,19 @@ export default function MapCard() {
       <CardBody>
         <div className="flex flex-col justify-center gap-3 items-center">
           <div className="font-medium self-start">Location</div>
-          <div className="rounded-lg overflow-hidden border-solid border-2 border-indigo-600 ">
+          <div className="rounded-lg overflow-hidden">
             <GoogleMapsEmbed
-              apiKey="AIzaSyBvSmftem94fcOEtNH9qUDn9ESrw-8AtHk"
+              //   apiKey="AIzaSyBvSmftem94fcOEtNH9qUDn9ESrw-8AtHk"
+              apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
               height={130}
               width={268}
               mode="place"
               q="Brooklyn+Bridge,New+York,NY"
             />
           </div>
-          <div className="self-start text-sm text-default-400 ">123, Jalan Medan Ipoh, Medan Ipoh Bestari, 99999 Ipoh, Perak</div>
-
+          <div className="self-start text-sm text-default-400 ">
+            123, Jalan Medan Ipoh, Medan Ipoh Bestari, 99999 Ipoh, Perak
+          </div>
         </div>
       </CardBody>
     </Card>
