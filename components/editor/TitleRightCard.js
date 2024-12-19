3af818e0680054c861a19d7d2871d1b9d56e3597
LAICHEEHOONG <laicheehoong@gmail.com>
2024-12-19 22:20:21 +0800

    title

--- components/editor/TitleRightCard.js
+++ components/editor/TitleRightCard.js
@@ -1,15 +1,46 @@
-import { Input, Divider, Button } from "@nextui-org/react";
-import { useSelector } from "react-redux";
+import { Input, Button } from "@nextui-org/react";
+import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
-import { IoIosAlert } from "react-icons/io";
+import ErrorIcon from "@mui/icons-material/Error";
+import { setAd, setAds } from "@/redux/features/editor/editorSlice";
+import { createAD, findUserAds } from "@/lib/action/adAction";
 
 export default function TitleRightCard() {
+  const dispatch = useDispatch();
   const ad = useSelector((state) => state.editor?.ad);
   const [title_, setTitle_] = useState(ad?.title);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     let inputValue = e.target.value;
     setTitle_(inputValue);
+  };
+
+  const fetchAds = async () => {
+    try {
+      const ads = await findUserAds({ user: ad.user }); // Pass only the userId
+      dispatch(setAds(ads));
+    } catch (error) {
+      console.error("Error fetching user ads:", error);
+    }
+  };
+
+  const toDB = async (adsId, newTitle) => {
+    try {
+      setLoading(true);
+      const updateTitle = await createAD({ ...ad, adsId, title: newTitle });
+      dispatch(setAd(updateTitle));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSave = () => {
+    const adsId = ad._id;
+    toDB(adsId, title_);
+    fetchAds();
   };
 
   return (
@@ -26,27 +57,26 @@ export default function TitleRightCard() {
         onChange={handleChange}
       />
       <div className="text-default-400 text-xs mt-5 mb-5">
-        {/* 20 characters available */}
-
         {title_.length <= 50 && `${50 - title_.length} characters available`}
         {title_.length > 50 && (
           <div className="flex gap-1 items-center text-red-600">
-            <IoIosAlert />
+            <ErrorIcon sx={{ fontSize: "1rem" }} />
             <div> {`${title_.length - 50} character over limit`} </div>
           </div>
         )}
       </div>
-      <div className="text-5xl font-semibold p-4 max-w-2xl text-center ">
+      <div className="text-5xl font-semibold p-4 max-w-2xl text-center text-ellipsis overflow-hidden ">
         {title_}
       </div>
 
-      {/* <Divider className="mt-20" /> */}
       <div className=" w-full flex p-10 justify-center items-center">
         <Button
           radius="full"
           size="lg"
           color="primary"
-          isDisabled={title_.length <= 50 ? false : true}
+          isDisabled={title_.length <= 50 && title_.length > 0 ? false : true}
+          isLoading={loading}
+          onPress={handleSave}
         >
           Save
         </Button>
