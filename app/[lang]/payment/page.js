e55ec5c6e6135acd03939c1275a8d2f19e25ccc8
LAICHEEHOONG <laicheehoong@gmail.com>
2025-01-08 13:29:41 +0800

    payment live

--- app/[lang]/payment/page.js
+++ app/[lang]/payment/page.js
@@ -3,7 +3,7 @@
 // import convertToSubcurrency from "@/lib/convertToSubcurrency";
 import CheckoutPage from "@/components/CheckoutPage";
 import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe} from "@stripe/stripe-js";
 
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
   throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
@@ -12,7 +12,7 @@ if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Payment() {
-  const amount = 9.99;
+  const amount = 2;
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
