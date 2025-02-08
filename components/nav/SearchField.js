733223de230ebc3996f7d4a7981781e49f985346
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2025-02-08 16:00:12 +0800

    search

--- components/nav/SearchField.js
+++ components/nav/SearchField.js
@@ -4,13 +4,28 @@ import { Autocomplete, AutocompleteItem, Button } from "@heroui/react";
 import SearchIcon from "@mui/icons-material/Search";
 import PlaceIcon from "@mui/icons-material/Place";
 import { animals } from "./data";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSearchValue } from "@/redux/features/search/searchSlice";
+import { getAreaSuggestions } from "@/lib/action/adAction";
 
 export default function SearchField({ navigation }) {
+
   const dispatch = useDispatch();
+  const searchValue = useSelector(state => state.search.value)
   const handleOnChange = (where) => {
     dispatch(setSearchValue(where))
+  }
+  const handleSearch = () => {
+    const getAreaSuggestions_ = async(query) => {
+      try {
+        const res = await getAreaSuggestions(query)
+        console.log(res)
+        // return res.data
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    getAreaSuggestions_(searchValue)
   }
 
   return (
@@ -33,7 +48,7 @@ export default function SearchField({ navigation }) {
           <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>
         )}
       </Autocomplete>
-      <Button isIconOnly color="danger" aria-label="Search Icon" radius="full">
+      <Button isIconOnly color="danger" aria-label="Search Icon" radius="full" onPress={handleSearch} >
         <SearchIcon className="w-5" />
       </Button>
     </div>
