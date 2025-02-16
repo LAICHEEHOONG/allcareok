1f34062b55f622c817c53abcfef72d71de0b66bb
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2025-02-16 17:11:20 +0800

    ad page

--- components/AllProvider.js
+++ components/AllProvider.js
@@ -6,15 +6,38 @@ import { HeroUIProvider } from "@heroui/react";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "./ui/sonner";
-import { useSession } from "next-auth/react";
-
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function AllProvider({ children }) {
+  // Method 1: Using usePathname hook (recommended)
+  const pathname = usePathname();
+  const [adNavSize, setAdNavSize] = useState(false);
+
+  // Method 2: Get search params
+
+  useEffect(() => {
+    // console.log("pathname", pathname);
+    // console.log("searchParams", searchParams);
+    if (pathname.includes("/ad/")) {
+      setAdNavSize(true);
+    } else {
+      setAdNavSize(false);
+    }
+    // console.log(pathname.includes('/ad/'))
+  }, [pathname.includes("/ad/")]);
+
   return (
     <Provider store={store}>
       <SessionProvider>
-        <HeroUIProvider>{children}</HeroUIProvider>
-        <Toaster position="top-center"  />
+        <HeroUIProvider>
+          <div className="w-full flex justify-center">
+            <div className={`w-full ${adNavSize && "max-w-[1120px] "}`}>
+              {children}
+            </div>
+          </div>
+        </HeroUIProvider>
+        <Toaster position="top-center" />
       </SessionProvider>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
     </Provider>
