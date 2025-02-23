e104a14ca1269cbfaa98fca83367cb998c808af5
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2025-02-23 17:21:06 +0800

    ad loading

--- components/Home/AD.js
+++ components/Home/AD.js
@@ -83,58 +83,56 @@ export default function AD({ ad, fn, adsId }) {
 
   return (
     // <Fade triggerOnce>
-      <Card className="w-full max-w-[350px] rounded-xl">
-        <CardHeader className="flex justify-center h-[52px]">
-          {(ad?.area?.town ||
-            ad?.area?.city ||
-            ad?.area?.state ||
-            ad?.area?.country) && (
-            <div className="flex justify-center items-center gap-2">
-              {ad?.area?.country ? (
-                <Avatar
-                  src={
-                    countryFlag.find(
-                      (country) =>
-                        country.value.trim().toLowerCase() ===
-                        ad.area.country.trim().toLowerCase()
-                    )?.description || ""
-                  }
-                  alt={ad.area.country}
-                  className="max-w-5 h-5 w-full"
-                />
-              ) : (
-                <LocationOnIcon className="w-4 h-4 mt-1" />
-              )}
+    <Card className="w-full max-w-[350px] rounded-xl">
+      <CardHeader className="flex justify-center h-[52px]">
+        {(ad?.area?.town ||
+          ad?.area?.city ||
+          ad?.area?.state ||
+          ad?.area?.country) && (
+          <div className="flex justify-center items-center gap-2">
+            {ad?.area?.country ? (
+              <Avatar
+                src={
+                  countryFlag.find(
+                    (country) =>
+                      country.value.trim().toLowerCase() ===
+                      ad.area.country.trim().toLowerCase()
+                  )?.description || ""
+                }
+                alt={ad.area.country}
+                className="max-w-5 h-5 w-full"
+              />
+            ) : (
+              <LocationOnIcon className="w-4 h-4 mt-1" />
+            )}
 
-              <div className="text-base capitalize font-medium w-full max-w-[220px] truncate tracking-widest">
-                {`${ad?.area?.town || ad?.area?.city || ad?.area?.state || ""}${
-                  ad?.area?.town || ad?.area?.city || ad?.area?.state
-                    ? ", "
-                    : ""
-                }${ad?.area?.country}`}
-              </div>
+            <div className="text-base capitalize font-medium w-full max-w-[220px] truncate tracking-widest">
+              {`${ad?.area?.town || ad?.area?.city || ad?.area?.state || ""}${
+                ad?.area?.town || ad?.area?.city || ad?.area?.state ? ", " : ""
+              }${ad?.area?.country}`}
             </div>
-          )}
-        </CardHeader>
-        <CardBody className="overflow-visible p-3 pb-1 pt-0">
-          <Carousel
-            className="w-full cursor-pointer"
-            opts={{ align: "start", loop: true, dragFree: false }}
-            // onClick={() => fn()}
-            onClick={() => {
-              if (typeof fn === "function") {
-                fn();
-              }
-            }}
-          >
-            <CarouselContent>
-              {ad?.photo?.length > 0 ? (
-                ad?.photo?.map((item, i) => (
-                  <CarouselItem
-                    key={`${crypto.randomUUID()}`}
-                    className="flex justify-center items-start"
-                  >
-                    {/* <div className="flex justify-center items-start h-full w-full">
+          </div>
+        )}
+      </CardHeader>
+      <CardBody className="overflow-visible p-3 pb-1 pt-0">
+        <Carousel
+          className="w-full cursor-pointer"
+          opts={{ align: "start", loop: true, dragFree: false }}
+          // onClick={() => fn()}
+          onClick={() => {
+            if (typeof fn === "function") {
+              fn();
+            }
+          }}
+        >
+          <CarouselContent>
+            {ad?.photo?.length > 0 ? (
+              ad?.photo?.map((item, i) => (
+                <CarouselItem
+                  key={`${crypto.randomUUID()}`}
+                  className="flex justify-center items-start"
+                >
+                  {/* <div className="flex justify-center items-start h-full w-full">
                         <Image
                           alt="ads image"
                           className={`object-cover rounded-xl 
@@ -153,20 +151,20 @@ export default function AD({ ad, fn, adsId }) {
                           src={item.url}
                         />
                       </div> */}
-                    <Image
-                      className={`object-cover rounded-xl w-[310px] xl:h-[400px] x1128l:h-[350px] sm:h-[400px] 550px:h-[320px] h-[400px]`}
-                      src={item.url}
-                      alt="ad image"
-                    />
-                  </CarouselItem>
-                ))
-              ) : (
-                <CarouselItem
-                  className="flex justify-center items-start"
+                  <Image
+                    className={`object-cover rounded-xl w-[310px] xl:h-[400px] x1128l:h-[350px] sm:h-[400px] 550px:h-[320px] h-[400px]`}
+                    src={item.url}
+                    alt="ad image"
+                  />
+                </CarouselItem>
+              ))
+            ) : (
+              <CarouselItem
+                className="flex justify-center items-start"
 
-                  // className="flex justify-center items-start h-full"
-                >
-                  {/* <Image
+                // className="flex justify-center items-start h-full"
+              >
+                {/* <Image
                     alt="default ad image"
                     className={`object-cover rounded-xl 
                       w-[333px] h-[400px]
@@ -183,150 +181,146 @@ export default function AD({ ad, fn, adsId }) {
                     radius="lg"
                     src="/images/plumber.png"
                   /> */}
-                  <Image
-                    className={`object-cover rounded-xl w-[310px] xl:h-[400px] x1128l:h-[350px] sm:h-[400px] 550px:h-[320px] h-[400px]`}
-                    // className={`object-cover rounded-xl w-[310px] xl:h-[400px] x1128l:h-[350px] sm:h-[400px] 550px:h-[320px] h-[400px]`}
-                    src="/images/plumber.png"
-                    alt="ad image"
+                <Image
+                  className={`object-cover rounded-xl w-[310px] xl:h-[400px] x1128l:h-[350px] sm:h-[400px] 550px:h-[320px] h-[400px]`}
+                  // className={`object-cover rounded-xl w-[310px] xl:h-[400px] x1128l:h-[350px] sm:h-[400px] 550px:h-[320px] h-[400px]`}
+                  src="/images/plumber.png"
+                  alt="ad image"
+                />
+              </CarouselItem>
+            )}
+          </CarouselContent>
+        </Carousel>
+        {showStatus ? (
+          <div
+            className={`absolute inset-x-0 top-0 z-30 flex ${
+              ad.reviewStatus !== "Payment Pending"
+                ? "justify-between"
+                : "justify-end"
+            } items-center p-1 pl-6 pr-6`}
+          >
+            {ad.reviewStatus !== "Payment Pending" && (
+              <Chip
+                avatar={
+                  <Avatar
+                    name="allcareok"
+                    src="https://www.allcareok.com/images/allcareok_logo.png"
                   />
-                </CarouselItem>
-              )}
+                }
+                variant="shadow"
+                classNames={{
+                  base:
+                    ad.reviewStatus === "Approved"
+                      ? "bg-green-400"
+                      : ad.reviewStatus === "Under Review"
+                      ? "bg-indigo-400"
+                      : ad.reviewStatus === "Rejected"
+                      ? "bg-pink-400 "
+                      : "bg-default",
+                  content: "drop-shadow shadow-black text-white",
+                }}
+              >
+                <div className="font-medium tracking-wider">
+                  {ad.reviewStatus === "Approved" && l?.approved}
+                  {ad.reviewStatus === "Under Review" && l?.under_review}
+                  {ad.reviewStatus === "Rejected" && l?.rejected}
+                  {/* {l?.verified} */}
+                </div>
+              </Chip>
+            )}
+
+            <Button
+              isIconOnly
+              aria-label="Like"
+              size="sm"
+              radius="full"
+              color="danger"
+              variant={isInWishlist(ad._id) ? "solid" : "flat"} // Change based on wishlist
+              isLoading={loadingAd[ad._id] || false} // Only the clicked button will show loading
+              onPress={() => updateUserWishlist_(ad._id)} // Handle wishlist update
+            >
+              <FavoriteBorderIcon style={{ color: "white" }} />
+            </Button>
+          </div>
+        ) : (
+          <div
+            className={`absolute inset-x-0 top-0 z-30 flex ${
+              ad.reviewStatus === "Approved" ? "justify-between" : "justify-end"
+            } items-center p-1 pl-6 pr-6`}
+          >
+            {ad.reviewStatus === "Approved" && (
+              <Chip
+                avatar={
+                  <Avatar
+                    name="allcareok"
+                    src="https://www.allcareok.com/images/allcareok_logo.png"
+                  />
+                }
+                variant="shadow"
+                classNames={{
+                  base: "bg-gradient-to-br from-indigo-500 to-pink-500  shadow-pink-500/30",
+                  content: "drop-shadow shadow-black text-white",
+                }}
+              >
+                <div className="font-medium tracking-wider">{l?.verified}</div>
+              </Chip>
+            )}
+
+            <Button
+              isIconOnly
+              aria-label="Like"
+              size="sm"
+              radius="full"
+              color="danger"
+              variant={isInWishlist(ad._id) ? "solid" : "flat"} // Change based on wishlist
+              isLoading={loadingAd[ad._id] || false} // Only the clicked button will show loading
+              onPress={() => updateUserWishlist_(ad._id)} // Handle wishlist update
+            >
+              <FavoriteBorderIcon style={{ color: "white" }} />
+            </Button>
+          </div>
+        )}
+
+        {adsId && ad?._id && adsId === ad._id ? (
+          <>
+            <div className="absolute inset-0 bg-pink-300 bg-opacity-50 rounded-xl z-40 mx-3"></div>
+            <div className="absolute inset-0 flex items-center justify-center z-40">
+              <LogoSpinner text={false} />
+            </div>
+          </>
+        ) : null}
+      </CardBody>
+      <CardFooter className="flex justify-center p-0 ">
+        <div className="w-full flex justify-center items-center max-w-[300px] h-[35px] ">
+          <Carousel
+            className="w-full max-w-[400px] "
+            opts={{ align: "start", loop: true, dragFree: false }}
+            plugins={[plugin.current]}
+          >
+            <CarouselContent className="-ml-1">
+              {ad?.service?.map((serv, i) => {
+                const match = carouselItems.find((item) => item?.id === serv);
+                return match ? (
+                  <CarouselItem
+                    key={`${crypto.randomUUID()}`}
+                    className="pl-1 basis-1/7 cursor-pointer group select-none z-30 flex justify-center items-center w-full max-w-[300px]"
+                  >
+                    <Fade>
+                      <div className="flex justify-center items-center gap-2 m-1 mr-2">
+                        <match.icon className="!w-6 !h-6 text-default-400" />
+                        <div className="font-light w-full max-w-[200px] truncate tracking-widest text-default-400">
+                          {match?.label}
+                        </div>
+                      </div>
+                    </Fade>
+                  </CarouselItem>
+                ) : null;
+              })}
             </CarouselContent>
           </Carousel>
-          {showStatus ? (
-            <div
-              className={`absolute inset-x-0 top-0 z-30 flex ${
-                ad.reviewStatus !== "Payment Pending"
-                  ? "justify-between"
-                  : "justify-end"
-              } items-center p-1 pl-6 pr-6`}
-            >
-              {ad.reviewStatus !== "Payment Pending" && (
-                <Chip
-                  avatar={
-                    <Avatar
-                      name="allcareok"
-                      src="https://www.allcareok.com/images/allcareok_logo.png"
-                    />
-                  }
-                  variant="shadow"
-                  classNames={{
-                    base:
-                      ad.reviewStatus === "Approved"
-                        ? "bg-green-400"
-                        : ad.reviewStatus === "Under Review"
-                        ? "bg-indigo-400"
-                        : ad.reviewStatus === "Rejected"
-                        ? "bg-pink-400 "
-                        : "bg-default",
-                    content: "drop-shadow shadow-black text-white",
-                  }}
-                >
-                  <div className="font-medium tracking-wider">
-                    {ad.reviewStatus === "Approved" && l?.approved}
-                    {ad.reviewStatus === "Under Review" && l?.under_review}
-                    {ad.reviewStatus === "Rejected" && l?.rejected}
-                    {/* {l?.verified} */}
-                  </div>
-                </Chip>
-              )}
-
-              <Button
-                isIconOnly
-                aria-label="Like"
-                size="sm"
-                radius="full"
-                color="danger"
-                variant={isInWishlist(ad._id) ? "solid" : "flat"} // Change based on wishlist
-                isLoading={loadingAd[ad._id] || false} // Only the clicked button will show loading
-                onPress={() => updateUserWishlist_(ad._id)} // Handle wishlist update
-              >
-                <FavoriteBorderIcon style={{ color: "white" }} />
-              </Button>
-            </div>
-          ) : (
-            <div
-              className={`absolute inset-x-0 top-0 z-30 flex ${
-                ad.reviewStatus === "Approved"
-                  ? "justify-between"
-                  : "justify-end"
-              } items-center p-1 pl-6 pr-6`}
-            >
-              {ad.reviewStatus === "Approved" && (
-                <Chip
-                  avatar={
-                    <Avatar
-                      name="allcareok"
-                      src="https://www.allcareok.com/images/allcareok_logo.png"
-                    />
-                  }
-                  variant="shadow"
-                  classNames={{
-                    base: "bg-gradient-to-br from-indigo-500 to-pink-500  shadow-pink-500/30",
-                    content: "drop-shadow shadow-black text-white",
-                  }}
-                >
-                  <div className="font-medium tracking-wider">
-                    {l?.verified}
-                  </div>
-                </Chip>
-              )}
-
-              <Button
-                isIconOnly
-                aria-label="Like"
-                size="sm"
-                radius="full"
-                color="danger"
-                variant={isInWishlist(ad._id) ? "solid" : "flat"} // Change based on wishlist
-                isLoading={loadingAd[ad._id] || false} // Only the clicked button will show loading
-                onPress={() => updateUserWishlist_(ad._id)} // Handle wishlist update
-              >
-                <FavoriteBorderIcon style={{ color: "white" }} />
-              </Button>
-            </div>
-          )}
-
-          {adsId && ad?._id && adsId === ad._id ? (
-            <>
-              <div className="absolute inset-0 bg-pink-300 bg-opacity-50 rounded-xl z-40"></div>
-              <div className="absolute inset-0 flex items-center justify-center z-40">
-                <LogoSpinner text={false} />
-              </div>
-            </>
-          ) : null}
-        </CardBody>
-        <CardFooter className="flex justify-center p-0 ">
-          <div className="w-full flex justify-center items-center max-w-[300px] h-[35px] ">
-            <Carousel
-              className="w-full max-w-[400px] "
-              opts={{ align: "start", loop: true, dragFree: false }}
-              plugins={[plugin.current]}
-            >
-              <CarouselContent className="-ml-1">
-                {ad?.service?.map((serv, i) => {
-                  const match = carouselItems.find((item) => item?.id === serv);
-                  return match ? (
-                    <CarouselItem
-                      key={`${crypto.randomUUID()}`}
-                      className="pl-1 basis-1/7 cursor-pointer group select-none z-30 flex justify-center items-center w-full max-w-[300px]"
-                    >
-                      <Fade>
-                        <div className="flex justify-center items-center gap-2 m-1 mr-2">
-                          <match.icon className="!w-6 !h-6 text-default-400" />
-                          <div className="font-light w-full max-w-[200px] truncate tracking-widest text-default-400">
-                            {match?.label}
-                          </div>
-                        </div>
-                      </Fade>
-                    </CarouselItem>
-                  ) : null;
-                })}
-              </CarouselContent>
-            </Carousel>
-          </div>
-        </CardFooter>
-      </Card>
+        </div>
+      </CardFooter>
+    </Card>
     // </Fade>
   );
 }
