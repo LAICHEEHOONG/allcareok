2285e2a0225efd8d107d124dc9920b11f8aa6f05
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2025-02-28 12:23:00 +0800

    not found to loading

--- app/[lang]/loading.js
+++ app/[lang]/loading.js
@@ -1,15 +1,8 @@
-'use client'
-// import { Spinner } from "@heroui/react";
+"use client";
 import { LogoSpinner } from "@/components/LogoSpinner";
 
 const Loading = () => {
   return (
-    // <div className="absolute inset-0 flex items-center justify-center dark text-foreground bg-background">
-
-    //   <div className="text-center" style={{ transform: "scale(2)" }}>
-    //     <Spinner color="danger" size="lg" />
-    //   </div>
-    // </div>
     <div className="h-screen w-full flex justify-center items-center">
       <LogoSpinner text={true} />
     </div>
