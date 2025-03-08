373bb4a9bfe21971b07885da689e55bc07ad70de
LAICHEEHOONG <laicheehoong@gmail.com>
2025-03-08 17:37:34 +0800

    ads

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -4,7 +4,7 @@ import AllProvider from "@/components/AllProvider";
 import Nav from "@/components/nav/Nav";
 import { i18n } from "@/i18n.config";
 import NavBottomWrap from "@/components/navBottom/NavBottomWrap";
-import Script from 'next/script';
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,12 +34,11 @@ export default async function RootLayout({ children, params, modal }) {
   return (
     <html lang={lang}>
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5418411936016586"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-        />
+          crossorigin="anonymous"
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-full flex justify-center sm:p-2 sm:pr-6 sm:pl-6`}
