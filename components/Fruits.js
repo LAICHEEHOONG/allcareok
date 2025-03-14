833a570c07483eed9ae611c7051efec2b8cc3c25
LAICHEEHOONG <laicheehoong@gmail.com>
2025-03-14 08:29:05 +0800

    add service

--- components/Fruits.js
+++ components/Fruits.js
@@ -1,76 +1,76 @@
-"use client";
+// "use client";
 
-import { Card, CardBody, CardFooter, Image} from "@heroui/react";
+// import { Card, CardBody, CardFooter, Image} from "@heroui/react";
 
-export default function Fruits() {
-  const list = [
-    {
-      title: "Orange",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$3.00",
-    },
-    {
-      title: "Raspberry",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3Dg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-      price: "$12.20",
-    },
-  ];
+// export default function Fruits() {
+//   const list = [
+//     {
+//       title: "Orange",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$5.50",
+//     },
+//     {
+//       title: "Tangerine",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$3.00",
+//     },
+//     {
+//       title: "Raspberry",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$10.00",
+//     },
+//     {
+//       title: "Lemon",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$5.30",
+//     },
+//     {
+//       title: "Avocado",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$15.70",
+//     },
+//     {
+//       title: "Lemon 2",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$8.00",
+//     },
+//     {
+//       title: "Banana",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3Dg",
+//       price: "$7.50",
+//     },
+//     {
+//       title: "Watermelon",
+//       img: "https://images.unsplash.com/photo-1453487021979-5b739b2849f4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+//       price: "$12.20",
+//     },
+//   ];
 
-  return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-      {list.map((item, index) => (
-        <Card
-          shadow="sm"
-          key={index}
-          isPressable
-          onPress={() => console.log("item pressed")}
-        >
-          <CardBody className="overflow-visible p-0">
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={item.title}
-              className="w-full object-cover h-[340px]"
-              src={item.img}
-            />
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <b>{item.title}</b>
-            <p className="text-default-500">{item.price}</p>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+//       {list.map((item, index) => (
+//         <Card
+//           shadow="sm"
+//           key={index}
+//           isPressable
+//           onPress={() => console.log("item pressed")}
+//         >
+//           <CardBody className="overflow-visible p-0">
+//             <Image
+//               shadow="sm"
+//               radius="lg"
+//               width="100%"
+//               alt={item.title}
+//               className="w-full object-cover h-[340px]"
+//               src={item.img}
+//             />
+//           </CardBody>
+//           <CardFooter className="text-small justify-between">
+//             <b>{item.title}</b>
+//             <p className="text-default-500">{item.price}</p>
+//           </CardFooter>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// }
--- components/carouselItems.js
+++ components/carouselItems.js
@@ -1,99 +1,3 @@
-// // carouselItems.js
-// import { GiBroom } from "react-icons/gi";
-// import { TbAirConditioning } from "react-icons/tb";
-// import HouseIcon from "@mui/icons-material/House";
-// import ChairIcon from "@mui/icons-material/Chair";
-// import BedIcon from "@mui/icons-material/Bed";
-// import { GiTheaterCurtains } from "react-icons/gi";
-// import LocalCarWashIcon from "@mui/icons-material/LocalCarWash";
-// import { BiSolidVirusBlock } from "react-icons/bi";
-// import ForestIcon from "@mui/icons-material/Forest";
-// import HandymanIcon from "@mui/icons-material/Handyman";
-// import FormatPaintIcon from "@mui/icons-material/FormatPaint";
-// import { FaBox } from "react-icons/fa";
-// import { GiFloorPolisher } from "react-icons/gi";
-// import PestControlIcon from "@mui/icons-material/PestControl";
-// import ElderlyWomanIcon from "@mui/icons-material/ElderlyWoman";
-// import { LuBaby } from "react-icons/lu";
-// import { FaPeopleGroup } from "react-icons/fa6";
-
-// export const getCarouselItems = (service_type) => [
-//   {
-//     label: service_type.home_cleaning,
-//     icon: GiBroom,
-//     id: "home_cleaning",
-//   },
-//   {
-//     label: service_type.aircon_servicing,
-//     icon: TbAirConditioning,
-//     id: "aircon_servicing",
-//   },
-//   {
-//     label: service_type.move_in_out_cleaning,
-//     icon: HouseIcon,
-//     id: "move_in_out_cleaning",
-//   },
-//   { label: service_type.sofa_cleaning, icon: ChairIcon, id: "sofa_cleaning" },
-//   {
-//     label: service_type.mattress_cleaning,
-//     icon: BedIcon,
-//     id: "mattress_cleaning",
-//   },
-//   {
-//     label: service_type.curtain_cleaning,
-//     icon: GiTheaterCurtains,
-//     id: "curtain_cleaning",
-//   },
-//   {
-//     label: service_type.car_washing,
-//     icon: LocalCarWashIcon,
-//     id: "car_washing",
-//   },
-//   {
-//     label: service_type.disinfection_service,
-//     icon: BiSolidVirusBlock,
-//     id: "disinfection_service",
-//   },
-//   {
-//     label: service_type.landscaping_service,
-//     icon: ForestIcon,
-//     id: "landscaping_service",
-//   },
-//   {
-//     label: service_type.handyman_services,
-//     icon: HandymanIcon,
-//     id: "handyman_services",
-//   },
-//   {
-//     label: service_type.painting_services,
-//     icon: FormatPaintIcon,
-//     id: "painting_services",
-//   },
-//   { label: service_type.moving_service, icon: FaBox, id: "moving_service" },
-//   {
-//     label: service_type.flooring_services,
-//     icon: GiFloorPolisher,
-//     id: "flooring_services",
-//   },
-//   {
-//     label: service_type.pest_control,
-//     icon: PestControlIcon,
-//     id: "pest_control",
-//   },
-//   {
-//     label: service_type.elderly_care,
-//     icon: ElderlyWomanIcon,
-//     id: "elderly_care",
-//   },
-//   { label: service_type.child_care, icon: LuBaby, id: "child_care" },
-//   {
-//     label: service_type.full_time_maid,
-//     icon: FaPeopleGroup,
-//     id: "full_time_maid",
-//   },
-// ];
-
-
 // carouselItems.js
 import { GiBroom } from "react-icons/gi";
 import { TbAirConditioning } from "react-icons/tb";
@@ -112,6 +16,33 @@ import PestControlIcon from "@mui/icons-material/PestControl";
 import ElderlyWomanIcon from "@mui/icons-material/ElderlyWoman";
 import { LuBaby } from "react-icons/lu";
 import { FaPeopleGroup } from "react-icons/fa6";
+import { LiaUserNurseSolid } from "react-icons/lia";
+import { GiFamilyHouse } from "react-icons/gi";
+import { MdOutlinePets } from "react-icons/md";
+
+import { MdElectricBolt } from "react-icons/md";
+import { MdPlumbing } from "react-icons/md";
+import { MdOutlineRoofing } from "react-icons/md";
+import { MdOutlineCarpenter } from "react-icons/md";
+import { GiCrackedGlass } from "react-icons/gi";
+import { CiUnlock } from "react-icons/ci";
+import { GiAutoRepair } from "react-icons/gi";
+import { MdOutlineDesignServices } from "react-icons/md";
+import { GiFoodTruck } from "react-icons/gi";
+import { MdOutlineSecurity } from "react-icons/md";
+import { PiSecurityCamera } from "react-icons/pi";
+import { MdOutlineLocalLaundryService } from "react-icons/md";
+import { FaBaby } from "react-icons/fa";
+import { TbMassage } from "react-icons/tb";
+import { BiTaxi } from "react-icons/bi";
+import { GiTeacher } from "react-icons/gi";
+import { FaComputer } from "react-icons/fa6";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { MdOutlinePool } from "react-icons/md";
+
+
+
+import { RiSpyLine } from "react-icons/ri";
 
 export const getCarouselItems = (service_type, service_type_description) => [
   {
@@ -215,5 +146,23 @@ export const getCarouselItems = (service_type, service_type_description) => [
     description: service_type_description?.full_time_maid,
     icon: FaPeopleGroup,
     id: "full_time_maid",
+  },
+  {
+    label: service_type.nurse,
+    description: service_type_description?.nurse,
+    icon: LiaUserNurseSolid,
+    id: "nurse",
+  },
+  {
+    label: service_type.renovate,
+    description: service_type_description?.renovate,
+    icon: GiFamilyHouse,
+    id: "renovate",
+  },
+  {
+    label: service_type.pet_grooming,
+    description: service_type_description?.pet_grooming,
+    icon: MdOutlinePets,
+    id: "pet_grooming",
   },
 ];
